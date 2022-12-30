557c5f51aeb832d68071bf3ee6dc5ad30a1a6fac
YouCee <64170266+YouCee-INNOVATIONS@users.noreply.github.com>
2022-12-30 13:50:43 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link')
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+})
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+/* Stack overflow */
+$(window).on("scroll", function() {
+    var scrollPos = $(window).scrollTop();
+    if (scrollPos <= 0) {
+        $('.navbar-default').addClass('top-of-page');
+    } else {
+        $('.navbar-default').removeClass('top-of-page');
+    }
+});
