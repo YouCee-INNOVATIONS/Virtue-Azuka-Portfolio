26b080d16fe10bd7e52ab42230e58bf73eacb214
YouCee <64170266+YouCee-INNOVATIONS@users.noreply.github.com>
2022-12-31 19:15:28 +0000

    about header changed

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__link')
-
-navToggle.addEventListener('click', () => {
-    document.body.classList.toggle('nav-open');
-});
-
-navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        document.body.classList.remove('nav-open');
-    })
-})
-
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
